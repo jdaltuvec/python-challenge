afe5f8d0358d195e79697317be1db00b03a2e9e8
David A <josedavac@gmail.com>
2019-12-01 20:31:51 -0600

    adding finished exercise to repo

--- PyBank/main.py
+++ PyBank/main.py
@@ -25,11 +25,10 @@ total = sum(int(row[1]) for row in data)
 profit_losses = [x[1] for x in data]
 profit_losses = [int(i) for i in profit_losses] # Transforming everything to ints
 
-i = 0
-while i < len(profit_losses)-1:
+
+for i in range(0, len(profit_losses)-1):
 	diff = profit_losses[i+1] - profit_losses[i]
 	changes.append(diff)
-	i = i + 1
 
 # Actual average
 average_diff = sum(changes) / len(changes)
@@ -37,22 +36,25 @@ average_diff = round(average_diff, 2)
 
 
 # The greatest increase in profits (date and amount) over the entire period
-profit = data[profit_losses.index(max(profit_losses))]
+profit = max(changes)
+profit_place = data[profit_losses.index(max(profit_losses))]
 
 
 # The greatest decrease in losses (date and amount) over the entire period
-loss = data[profit_losses.index(min(profit_losses))]
+loss = min(changes)
+loss_place = data[profit_losses.index(min(profit_losses))]
 
 
 # In addition, your final script should both print the analysis to the terminal and export a text file with the results.
 
-analysis = "Financial Analysis"\
+analysis = "\n"+"\n"\
++ "Financial Analysis"\
 +"\n"+"----------------------------"\
 +"\n"+"Total Months: " + str(months)\
 +"\n"+"Total: $" + str(total)\
 +"\n"+"Average  Change: $" + str(average_diff)\
-+"\n"+"Greatest Increase in Profits: " + str(profit[0]) + " ($" + str(profit[1]) +")"\
-+"\n"+"Greatest Decrease in Profits: " + str(loss[0]) + " ($" + str(loss[1]) +")"
++"\n"+"Greatest Increase in Profits: " + str(profit) + " ($" + str(profit_place[0]) +")"\
++"\n"+"Greatest Decrease in Profits: " + str(loss) + " ($" + str(profit_place[0]) +")"
 
 print(analysis)
 
